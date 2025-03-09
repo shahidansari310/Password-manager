e4aea977b880f70715a07ee7d6323c170fa450db
Shahid Ansari <113099652+shahidansari310@users.noreply.github.com>
2025-03-10 02:44:04 +0530

    creating Navbar

--- KeyHeaven/src/App.jsx
+++ KeyHeaven/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <>
-      
+    <Navbar/>
+      <div>
+        My Ui
+      </div>
     </>
   )
 }
---
+++ KeyHeaven/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='fixed bg-purple-500 p-4'>
+        <div className=''></div>
+      <ul>
+        <li>
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar

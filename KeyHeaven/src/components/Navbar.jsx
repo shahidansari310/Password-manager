10ea2533460d148d64329f86af683e643729efb7
Shahid Ansari <113099652+shahidansari310@users.noreply.github.com>
2025-03-10 16:06:32 +0530

    navbar editing

--- KeyHeaven/src/components/Navbar.jsx
+++ KeyHeaven/src/components/Navbar.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between bg-purple-500 p-4">
+    <nav className="flex justify-between bg-purple-500 p-2">
         <div className='font-bold text-xl'>
             &lt;KeyHeaven/&gt;
             </div>
       <ul>
-        <li className="flex gap-4">
-            <a href="/"><img src="/home.svg" alt="Home Icon" className="w-6 h-6"/>
-            </a>
-            <a href="/">About</a>
-            <a href="/">Contact</a>
-        </li>
+      <li className="flex ">
+            <a href=""><img src="/git.png" alt="" className=' mx-2 h-8 border rounded-full'/></a>
+            <a href=""><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRokEYt0yyh6uNDKL8uksVLlhZ35laKNQgZ9g&s" alt="" className=' mx-2 h-8 rounded-full'/></a>
+            <a href=""><img src="https://img.freepik.com/free-vector/twitter-app-new-logo-x-black-background_1017-45425.jpg?semt=ais_hybrid" alt="" className=' mx-2 h-8 rounded-full'/></a>
+        </li> 
       </ul>
     </nav>
   )
